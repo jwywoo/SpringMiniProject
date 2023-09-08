2d1810a206811750bd46d1a89c05bb5d717f7fab
jwywoo <jwywoo26@gmail.com>
2023-09-09 02:38:38 +0900

    3rd Commit PostCommentAuth Mini Project: Member SignUp Done

--- PostCommentAuth/src/main/java/com/example/postcommentauth/member/controller/MemberController.java
+++ PostCommentAuth/src/main/java/com/example/postcommentauth/member/controller/MemberController.java
@@ -1,4 +1,24 @@
 package com.example.postcommentauth.member.controller;
 
+import com.example.postcommentauth.member.dto.SignupRequestDto;
+import com.example.postcommentauth.member.dto.StringResponseDto;
+import com.example.postcommentauth.member.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<StringResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto) {
+        return ResponseEntity.ok(memberService.signup(requestDto));
+    }
 }
--- PostCommentAuth/src/main/java/com/example/postcommentauth/member/service/MemberService.java
+++ PostCommentAuth/src/main/java/com/example/postcommentauth/member/service/MemberService.java
@@ -1,4 +1,35 @@
 package com.example.postcommentauth.member.service;
 
+import com.example.postcommentauth.common.JwtUtil;
+import com.example.postcommentauth.member.dto.SignupRequestDto;
+import com.example.postcommentauth.member.dto.StringResponseDto;
+import com.example.postcommentauth.member.entity.Member;
+import com.example.postcommentauth.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+
+    public StringResponseDto signup(SignupRequestDto requestDto) {
+        String username = requestDto.getUsername();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+
+        // Duplication Check
+        Optional<Member> memberCheck = memberRepository.findByUsername(username);
+        if (memberCheck.isPresent()) {
+            throw new IllegalArgumentException("중복된 아이디");
+        }
+
+        Member newMember = new Member(username, password);
+        memberRepository.save(newMember);
+        return new StringResponseDto("회원가입 성공!","200");
+    }
 }
